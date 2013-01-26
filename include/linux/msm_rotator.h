9183090aa69ba6cc8b3bd84f69e859c001fbe403
Flemmard <flemmard@gmail.com>
2013-01-26 15:33:04 +0100

    Added one unknown flag on rotator struct, fixes glitches on 3d games, and logcat spam

--- include/linux/msm_rotator.h
+++ include/linux/msm_rotator.h
@@ -31,7 +31,8 @@ struct msm_rotator_img_info {
 	unsigned char   rotations;
 	int enable;
 	unsigned int	downscale_ratio;
-	unsigned int secure;
+        unsigned int secure;
+        unsigned int unknown;
 };
 
 struct msm_rotator_data_info {
